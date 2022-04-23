4e10a48c5006fcbc8e0d726b4bc4bf240ec71f0a
nasirdiu <nasir.joypurhat@gmail.com>
2022-04-24 01:01:07 +0600

    aaaaaaa

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -44,6 +44,17 @@ async function run() {
     app.get("/productCount", async (req, res) => {
       const count = await productCollection.estimatedDocumentCount();
       res.send({ count }); //res.json(count); ai vabe kora
+    });
+
+    //use post
+
+    app.post("/productByKeys", async (req, res) => {
+      const keys = req.body;
+      const ids=keys.map(id=>ObjectId(id));
+      const query = { _id: { $in: ids } };
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
     });
   } finally {
   }
